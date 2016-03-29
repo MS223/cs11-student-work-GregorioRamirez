237402616ecb8765e05eca0fb75103e29e79e551
GregorioRamirez <gregorio.ramirez@ms223.org>
2016-03-29 08:48:30 -0400

    Create CountdownV2.py

---
+++ CountdownV2.py
@@ -0,0 +1,5 @@
+x = input("Give me a number") # x is the input called give me a number
+while x > 0: # x is greater than 0
+    print x # prints x 
+    x = x - 1 # it will be true until x doesn't equal x - 1 
+print "BOOM!" # prints BOOM! when it hits to 0 
