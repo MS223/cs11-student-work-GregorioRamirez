6512a0074154f088dd17f2a088fa10adb769a79e
GregorioRamirez <gregorio.ramirez@ms223.org>
2016-04-04 08:40:51 -0400

    Create Guessing.game.py

---
+++ Guessing.game.py
@@ -0,0 +1,5 @@
+name = raw_input("What's your name?")
+upper_bound = input("What's the biggest number you want to guess?")
+guess = input("What's your guess?")
+import random
+random_number = random.randint(0,upper_bound)
