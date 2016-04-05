d2959020b7d8b8be77c2ab01ab2cfba44532637d
GregorioRamirez <gregorio.ramirez@ms223.org>
2016-04-05 09:21:36 -0400

    Update Guessing.game.py

--- Guessing.game.py
+++ Guessing.game.py
@@ -3,3 +3,14 @@ upper_bound = input("What's the biggest number you want to guess?")
 guess = input("What's your guess?")
 import random
 random_number = random.randint(0,upper_bound)
+while guess != random_number:
+    if guess > random_number:
+        print "Your answer is too high," + str(name)
+        guess = input("What's your guess?")
+    else:
+        # guess < random
+        print "Your answer is too low, " + str(name)
+        guess = input("What's your guess?")
+    if guess == random_number:
+        print "CORRECT !"
+        
