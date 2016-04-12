ff4e0a0afb9244e3dc0874f9a4ca3c519cf8b136
GregorioRamirez <gregorio.ramirez@ms223.org>
2016-04-12 09:26:02 -0400

    Create War.py

---
+++ War.py
@@ -0,0 +1,11 @@
+player1 = raw_input("What's player 1's name?")
+player2 = raw_input("What's player 2's name?")
+
+
+import random
+def suffled_deck():
+    basic_deck = range(2, 15) * 4
+    random.shuffle(basic_deck)
+    return basic_deck
+
+def 
