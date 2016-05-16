1636679a225b307d6e02022502909fbdf9ca6316
GregorioRamirez <gregorio.ramirez@ms223.org>
2016-05-16 08:42:32 -0400

    Create Dictionary_Lab.py

---
+++ Dictionary_Lab.py
@@ -0,0 +1,17 @@
+user_profile = {
+'name' 
+'school'
+'birthday'
+'email'
+'username'
+'where are you from?'
+'number of siblings'
+'skills'
+'about you'
+'favorite quote'
+'favorite movie'
+'favorite musician'
+'favorite tv show'
+'favorite book'
+}
+print 
